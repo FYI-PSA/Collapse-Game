35ecba4b3b79a1f6616f5919dce4ad2c68b22e01
FYI-PSA <77334085+FYI-PSA@users.noreply.github.com>
2024-09-15 00:29:01 +0330

    Created main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/bin/python3.10
+
+
+class GameLogic:
+    def __init__(self) -> None:
+        return
+
+
+class GameRenderer:
+    def __init__(self) -> None:
+        return
+
+
+def main() -> None:
+    return
+
+
+if __name__ == '__main__':
+    main()
